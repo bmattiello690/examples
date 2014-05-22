9f2cf5c9c4d715a2bd71aabf1c2df644679fd97c
bmattiello690 <bmattiello@agorapublishinggroup.com>
2014-05-22 09:35:57 -0400

    changed print location
    changed 1 to 2

--- chapter4_examples/ex4.6.php
+++ chapter4_examples/ex4.6.php
@@ -2,5 +2,7 @@
 if ( 0 == False && "" == FALSE) {
 print "zero and null are <em>false.</em><br> ";}
 if ( 1 == True && "abc" == true) {
-print "1 and \"abc\" are both <em>true.</em><br> "; }
+print "2 and \"abc\" are both <em>true.</em><br> "; }
+
+
 ?>
